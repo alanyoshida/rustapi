791962502797a857cae85526d59b1b07e363dc5d
alan.oliveira <alan.oliveira@dafiti.com.br>
2021-05-13 21:48:24 -0300

    logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+mod logger;
+
+type StdErr = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), StdErr> {
+    // loads env variables from .env
+    dotenv::dotenv()?;
+    logger::init()?;
+
+    // example
+    assert_eq!("INFO", std::env::var("LOG_LEVEL").unwrap());
+
+    Ok(())
 }
